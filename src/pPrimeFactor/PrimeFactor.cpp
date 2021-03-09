992299c3f77be1953ff8d8297d817572e33c6be2
ray0727 <rayhuang100@gmail.com>
2021-03-09 14:56:16 +0800

    add podom to mission

--- src/pPrimeFactor/PrimeFactor.cpp
+++ src/pPrimeFactor/PrimeFactor.cpp
@@ -1,41 +1,35 @@
 /************************************************************/
 /*    NAME: Ray Huang                                              */
 /*    ORGN: MIT, Cambridge MA                               */
-/*    FILE: Odometry.cpp                                        */
+/*    FILE: PrimeFactor.cpp                                        */
 /*    DATE: December 29th, 1963                             */
 /************************************************************/
 
 #include <iterator>
 #include "MBUtils.h"
 #include "ACTable.h"
-#include "Odometry.h"
+#include "PrimeFactor.h"
 
 using namespace std;
 
 //---------------------------------------------------------
 // Constructor
 
-Odometry::Odometry()
+PrimeFactor::PrimeFactor()
 {
-  m_first_reading = true;
-  m_current_x = 0;
-  m_current_y = 0;
-  m_previous_x = 0;
-  m_previous_y = 0;
-  m_total_distance = 0;
 }
 
 //---------------------------------------------------------
 // Destructor
 
-Odometry::~Odometry()
+PrimeFactor::~PrimeFactor()
 {
 }
 
 //---------------------------------------------------------
 // Procedure: OnNewMail
 
-bool Odometry::OnNewMail(MOOSMSG_LIST &NewMail)
+bool PrimeFactor::OnNewMail(MOOSMSG_LIST &NewMail)
 {
   AppCastingMOOSApp::OnNewMail(NewMail);
 
@@ -43,21 +37,6 @@ bool Odometry::OnNewMail(MOOSMSG_LIST &NewMail)
   for(p=NewMail.begin(); p!=NewMail.end(); p++) {
     CMOOSMsg &msg = *p;
     string key    = msg.GetKey();
-    double value = msg.GetDouble();
-
-    if(key == "NAV_X")
-    {
-      m_first_reading = true;
-      m_previous_x = m_current_x;
-      m_current_x = value;
-    }
-
-    if(key == "NAV_Y")
-    {
-      m_first_reading = true;
-      m_previous_y = m_current_y;
-      m_current_y = value;
-    }
 
 #if 0 // Keep these around just for template
     string comm  = msg.GetCommunity();
@@ -82,7 +61,7 @@ bool Odometry::OnNewMail(MOOSMSG_LIST &NewMail)
 //---------------------------------------------------------
 // Procedure: OnConnectToServer
 
-bool Odometry::OnConnectToServer()
+bool PrimeFactor::OnConnectToServer()
 {
    registerVariables();
    return(true);
@@ -92,16 +71,10 @@ bool Odometry::OnConnectToServer()
 // Procedure: Iterate()
 //            happens AppTick times per second
 
-bool Odometry::Iterate()
+bool PrimeFactor::Iterate()
 {
   AppCastingMOOSApp::Iterate();
-  if(m_first_reading){
-    m_total_distance += sqrt(pow((m_current_x-m_previous_x), 2)+pow((m_current_y - m_previous_y),2));
-    Notify("ODOMETRY_DIST", m_total_distance);
-  }
-  else{
-    m_total_distance = 0;
-  }
+  // Do your thing here!
   AppCastingMOOSApp::PostReport();
   return(true);
 }
@@ -110,7 +83,7 @@ bool Odometry::Iterate()
 // Procedure: OnStartUp()
 //            happens before connection is open
 
-bool Odometry::OnStartUp()
+bool PrimeFactor::OnStartUp()
 {
   AppCastingMOOSApp::OnStartUp();
 
@@ -146,21 +119,28 @@ bool Odometry::OnStartUp()
 //---------------------------------------------------------
 // Procedure: registerVariables
 
-void Odometry::registerVariables()
+void PrimeFactor::registerVariables()
 {
   AppCastingMOOSApp::RegisterVariables();
   // Register("FOOBAR", 0);
-  Register("NAV_X", 0);
-  Register("NAV_Y", 0);
 }
 
 
 //------------------------------------------------------------
 // Procedure: buildReport()
 
-bool Odometry::buildReport() 
+bool PrimeFactor::buildReport() 
 {
-  m_msgs << "Distance: " << m_total_distance << endl;
+  m_msgs << "============================================" << endl;
+  m_msgs << "File:                                       " << endl;
+  m_msgs << "============================================" << endl;
+
+  ACTable actab(4);
+  actab << "Alpha | Bravo | Charlie | Delta";
+  actab.addHeaderLines();
+  actab << "one" << "two" << "three" << "four";
+  m_msgs << actab.getFormattedString();
+
   return(true);
 }
 
